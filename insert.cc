93e2e0599bc28349c2e197bf3f79ff2af0744584
MJavaadAkhtar <javaad.akhtar@mail.utoronto.ca>
2018-11-18 17:43:37 -0500

    changes

--- insert.cc
+++ insert.cc
@@ -11,7 +11,6 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-
     std::ifstream csv_file;
     csv_file.open(argv[2]);
     if (!csv_file) {
@@ -26,7 +25,6 @@ int main(int argc, char** argv) {
         return 1;
     }
     int page_size = atoi(argv[3]);
-
     Heapfile *heap = new Heapfile();
     heap->page_size = page_size;
     heap->file_ptr = heapfile;
@@ -42,16 +40,11 @@ int main(int argc, char** argv) {
         csv_file >> line;
 
         if (line.size() == 0) {
-            // ignore empty lines
             continue;
         }
-
-        // remove all commas from the line
         line.erase(std::remove(line.begin(), line.end(), ','), line.end());
 
         Record *r = new Record;
-
-        // turn 'line' from string to char*, and read the values into r
         fixed_len_read((char*)line.c_str(), record_size, r);
 
         int slot_index = add_fixed_len_page(&page, r);
@@ -61,29 +54,26 @@ int main(int argc, char** argv) {
             buffer = new char[buf_size];
 
             std::vector<Record> *page_data = page.data;
-            for (int i = 0; i < fixed_len_page_capacity(&page); i++) {
+            for (int i = 0; i < fixed_len_page_capacity(&page); ++i) {
                 fixed_len_write(&page_data->at(i), buffer);
             }
 
             PageID new_pid = alloc_page(heap);
             write_page(&page, heap, new_pid);
 
-            // allocate empty page
             init_fixed_len_page(&page, page_size, record_size);
-            // recalculate slot index
             slot_index = add_fixed_len_page(&page, r);
         }
 
         write_fixed_len_page(&page, slot_index, r);
     }
 
-    // write last page to file if it has records
     if (page.used_slots > 0) {
         int buf_size = page.page_size * record_size;
         buffer = new char[buf_size];
 
         std::vector<Record> *page_data = page.data;
-        for (int i = 0; i < fixed_len_page_capacity(&page); i++) {
+        for (int i = 0; i < fixed_len_page_capacity(&page); ++i) {
             fixed_len_write(&page_data->at(i), buffer);
         }
 
